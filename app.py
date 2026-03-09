39d29b577266897e9f606cd96974226a789107f0
Zorzi007 <247672025@eniac.edu.br>
2026-03-09 18:46:29 -0300

    Banco de dados

--- app.py
+++ app.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route('/') # Define a URL principal
 def home(): # Função executada ao acessar
     return 'Bem vindo a Old-Tech-Store!' # Resposta para o usuário
+
+def conectar_banco(): # Função para conectar ao MySQL
+ conn = mysql.connector.connect(
+ host="localhost", # Servidor local
+ user="root", # Usuário do MySQL
+ password="sua_senha", # Senha do MySQL
+ database="ecommerce_db" # Nome do banco
+ )
+ return conn # Retorna a conexão
