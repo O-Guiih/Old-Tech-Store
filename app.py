19643ea1a220f5c2d49ba04a0c51e0cd14129f5b
O-Guiih <guiihsilvasousa1@gmail.com>
2026-03-09 20:10:15 -0300

    Correção_usuário

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import _mysql_connector
+import mysql.connector
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route('/') # Define a URL principal
 def home(): # Função executada ao acessar
     return 'Bem vindo a Old-Tech-Store!' # Resposta para o usuário
+
+def conectar_banco(): # Função para conectar ao MySQL
+ conn = mysql.connector.connect(
+ host="localhost", # Servidor local
+ user="root", # Usuário do MySQL
+ password="", # Senha do MySQL
+ database="ecommerce_db" # Nome do banco
+ )
+ return conn # Retorna a conexão
