f9170bb6b75ca930657f72fb70b9983298d1f0c1
O-Guiih <guiihsilvasousa1@gmail.com>
2026-03-10 19:37:26 -0300

    Rota de Cadastro e Formulário de Cadastro

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import mysql.connector
-from flask import Flask
+from flask import Flask, render_template
+from flask import request # importa request para pegar dados do formulário 
 
 app = Flask(__name__)
 
@@ -15,3 +16,31 @@ def conectar_banco(): # Função para conectar ao MySQL
  database="ecommerce_db" # Nome do banco
  )
  return conn # Retorna a conexão
+
+#ROTA DE CADASTRO
+@app.route("/cadastro", methods= ["GET", "POST"])
+def cadastro():
+   if request.method == "POST": #verifica se o formulário foi enviado
+      #Pega os dados do formulário
+      nome = request.form["nome"] #Pega o valor digitado no campo nome 
+      email = request.form["email"] #Pega o valor digitado no campo de email
+      senha = request.form["senha"] #Pega o valor digitado no campo de senha
+
+      #Conecta do banco MySQL
+      conn = conectar_banco()
+      cursor = conn.cursor()
+      
+      #Insere um novo usuário
+      cursor.execute(
+       "INSERT INTO usuarios (nome, email, senha) VALUES (%s, %s, %s)", # %s -> placeholder do MySQL (diferente do ? do SQLite) e INSERT INTO -> Salva o novo usuário no banco
+      (nome, email, senha)
+      )
+   
+      #Salva e fecha
+      conn.commit()
+      conn.close()
+
+      return "Cadastro realizado com sucesso!"
+
+#Se for GET, mostra o formulário
+   return render_template("cadastro.html")
